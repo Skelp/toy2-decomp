6e7911cec46b6b11ff293a317ce4857f7c8a4938
Skelp <skelp@pm.me>
2026-07-24 21:51:30 +0200

    Implement Toy2::KiteTail::Init/Activate/Deactivate (cluster, Activate+Deactivate 100%)

---
+++ src/Toy2/KiteTail.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "Numerics.h"
+
+namespace Toy2
+{
+	namespace KiteTail
+	{
+		struct Data
+		{
+			struct Segment
+			{
+				Vector3I pos;
+				int32_t reserved;
+			} segments[64];
+
+			int16_t velocities[64][4];
+
+			int32_t segmentCount;
+			int32_t spacing;
+			int32_t maxAngle;
+			uint8_t flags;
+		};
+
+		extern Data* g_kiteTails[8];
+
+		void Init(const Vector3I* pos, int32_t count, int32_t spacing, int32_t index, int32_t maxAngle);
+		void Activate(int32_t index);
+		void Deactivate(int32_t index);
+	}
+}
