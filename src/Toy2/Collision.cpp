01021c56881a028ea764d75edcf9914e9026872e
Skelp <skelp@pm.me>
2026-07-28 09:14:28 +0200

    Reconstruct platform state helpers

--- src/Toy2/Collision.cpp
+++ src/Toy2/Collision.cpp
@@ -13,4 +13,46 @@ namespace Toy2
 		// STUB: TOY2 0x00489C30
 		void BuildCollisionWorld(int32_t level, uint8_t** buffer, int32_t terrainNum) {}
 	}
+
+	namespace Platform
+	{
+		// GLOBAL: TOY2 0x0072872C
+		PlatformState g_platformStates[32];
+
+		// FUNCTION: TOY2 0x00488580 [MATCHED]
+		int32_t GetFlags(int32_t platformIndex) { return g_platformStates[platformIndex].flags; }
+
+		// FUNCTION: TOY2 0x00488A60 [MATCHED]
+		void AddFlags(int32_t platformIndex, uint16_t flags) { g_platformStates[platformIndex].flags |= flags; }
+
+		// FUNCTION: TOY2 0x00488A80 [MATCHED]
+		void ClearFlags(int32_t platformIndex, int32_t flags) { g_platformStates[platformIndex].flags &= ~flags; }
+
+		// FUNCTION: TOY2 0x00488AA0 [MATCHED]
+		void SetRotationAngles(int32_t platformIndex, int16_t x, int16_t y, int16_t z)
+		{
+			g_platformStates[platformIndex].rotationAnglesFixed.x = x * 4;
+			g_platformStates[platformIndex].rotationAnglesFixed.y = y * 4;
+			g_platformStates[platformIndex].rotationAnglesFixed.z = z * 4;
+			g_platformStates[platformIndex].flags |= 8;
+		}
+
+		// FUNCTION: TOY2 0x00488AF0 [MATCHED]
+		void GetRotationAngles(int32_t platformIndex, Vector3I* angles)
+		{
+			angles->x = g_platformStates[platformIndex].rotationAnglesFixed.x >> 2;
+			angles->y = g_platformStates[platformIndex].rotationAnglesFixed.y >> 2;
+			angles->z = g_platformStates[platformIndex].rotationAnglesFixed.z >> 2;
+		}
+
+		// FUNCTION: TOY2 0x0048B640 [MATCHED]
+		int32_t HadBuzzContactThisFrame(int32_t platformIndex)
+		{
+			if ((g_platformStates[platformIndex].flags & 2) != 0 || (g_platformStates[platformIndex].flags & 0x10) != 0)
+			{
+				return 1;
+			}
+			return 0;
+		}
+	}
 }
--- src/Toy2/Collision.h
+++ src/Toy2/Collision.h
@@ -3,6 +3,8 @@
 #include "Common.h"
 #include "Numerics.h"
 
+#include <stddef.h>
+
 namespace Toy2
 {
 	namespace Collision
@@ -19,11 +21,38 @@ namespace Toy2
 			int32_t boundingSqRadius;
 		};
 
-        extern CollisionMeshInstance g_collisionMeshInstances[300];
-        extern uint8_t g_mathScratch[1024];
+		extern CollisionMeshInstance g_collisionMeshInstances[300];
+		extern uint8_t g_mathScratch[1024];
 
-        void BuildCollisionWorld(int32_t level, uint8_t **buffer, int32_t terrainNum);
+		void BuildCollisionWorld(int32_t level, uint8_t** buffer, int32_t terrainNum);
 
 		STATIC_ASSERT(sizeof(CollisionMeshInstance) == 0x34);
+	}
+
+	namespace Platform
+	{
+		struct PlatformState
+		{
+			Vector3I16 rotationAnglesFixed;
+			int16_t motionState[13];
+			uint8_t* contactFace;
+			int16_t collisionMeshIndex;
+			int16_t flags;
+			uint8_t reserved[0xC];
+		};
+
+		int32_t GetFlags(int32_t platformIndex);
+		void AddFlags(int32_t platformIndex, uint16_t flags);
+		void ClearFlags(int32_t platformIndex, int32_t flags);
+		void SetRotationAngles(int32_t platformIndex, int16_t x, int16_t y, int16_t z);
+		void GetRotationAngles(int32_t platformIndex, Vector3I* angles);
+		int32_t HadBuzzContactThisFrame(int32_t platformIndex);
+
+		extern PlatformState g_platformStates[32];
+
+		STATIC_ASSERT(offsetof(PlatformState, contactFace) == 0x20);
+		STATIC_ASSERT(offsetof(PlatformState, collisionMeshIndex) == 0x24);
+		STATIC_ASSERT(offsetof(PlatformState, flags) == 0x26);
+		STATIC_ASSERT(sizeof(PlatformState) == 0x34);
 	}
 }
