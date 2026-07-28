1e6137455bfee92ae31843d1588a67a4fdd08273
Skelp <skelp@pm.me>
2026-07-28 10:17:32 +0200

    Reconstruct FMV instance destruction

--- src/Nu3D/FMV.cpp
+++ src/Nu3D/FMV.cpp
@@ -1,9 +1,32 @@
 #include "Nu3D/FMV.h"
+#include <stdlib.h>
 
 enum Nu3DFMVStateFlags
 {
 	NU3D_FMV_PAUSED = 4
 };
+
+// FUNCTION: TOY2 0x004DB8C0 [MATCHED]
+void Nu3D_FMV_Destroy(Nu3DFMVInstance* instance)
+{
+	if (instance->basicVideo != NULL)
+	{
+		instance->basicVideo->Release();
+	}
+	if (instance->videoWindow != NULL)
+	{
+		instance->videoWindow->Release();
+	}
+	if (instance->mediaEvent != NULL)
+	{
+		instance->mediaEvent->Release();
+	}
+	if (instance->mediaControl != NULL)
+	{
+		instance->mediaControl->Release();
+	}
+	free(instance);
+}
 
 // FUNCTION: TOY2 0x004DB910 [MATCHED]
 void Nu3D_FMV_SetDimensions(Nu3DFMVInstance* instance, int32_t left, int32_t top, int32_t right, int32_t bottom)
--- src/Nu3D/FMV.h
+++ src/Nu3D/FMV.h
@@ -2,15 +2,16 @@
 
 #include "Common.h"
 #include <stddef.h>
+#include <windows.h>
 
 struct Nu3DFMVInstance
 {
-	void* mediaControl;
-	void* mediaEvent;
-	void* videoWindow;
-	void* sourceSurface;
-	void* basicVideo;
-	void* renderSurface;
+	IUnknown* mediaControl;
+	IUnknown* mediaEvent;
+	IUnknown* videoWindow;
+	IUnknown* sourceSurface;
+	IUnknown* basicVideo;
+	IUnknown* renderSurface;
 	uint32_t stateFlags;
 	float left;
 	float top;
@@ -19,6 +20,7 @@ struct Nu3DFMVInstance
 };
 
 int32_t Nu3D_FMV_IsPlaying(Nu3DFMVInstance* instance);
+void Nu3D_FMV_Destroy(Nu3DFMVInstance* instance);
 void Nu3D_FMV_SetDimensions(Nu3DFMVInstance* instance, int32_t left, int32_t top, int32_t right, int32_t bottom);
 
 STATIC_ASSERT(offsetof(Nu3DFMVInstance, stateFlags) == 0x18);
