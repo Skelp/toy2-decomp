c0589de28a1fb79a26b171b4bf78e5a652ff0775
Skelp <skelp@pm.me>
2026-07-29 09:57:03 +0200

    Reconstruct CreatureBehaviour LTyke

--- src/Toy2/Actor.cpp
+++ src/Toy2/Actor.cpp
@@ -374,6 +374,26 @@ namespace Toy2
 			}
 		}
 
+		// FUNCTION: TOY2 0x0041BB80 [MATCHED]
+		void LTyke(Actor::Toy2Actor::ActorBehaviourContext* context)
+		{
+			Actor::Toy2Actor* actor = context->actor;
+			actor->previousActorPhase -= (int16_t)Renderer::g_frameDelta;
+			if (actor->previousActorPhase <= 0)
+			{
+				actor->previousActorPhase = (*g_randDatBufferPtr++ & 0x7F) + 0x5A;
+				AudioManager::PlaySoundEffect(0x6B, &actor->pos);
+			}
+
+			if ((actor->actorFlags & Actor::ACTOR_FLAG_INTERACTION_REQUESTED) != 0 && actor->actorPhase == 0x66)
+			{
+				g_levelObjectiveProgress++;
+				Particles::SpawnCollectSparkle(actor->pos.x, actor->pos.y - 0x2000, actor->pos.z, 0x32);
+				AudioManager::PlaySoundEffect(0x6C, &actor->pos);
+				Actor::Kill(actor, 2);
+			}
+		}
+
 		// STUB: TOY2 0x00416F30
 		void RCCarLevel1(Actor::Toy2Actor::ActorBehaviourContext* context) {}
 
