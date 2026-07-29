c608334bf16e23ac8d25bd55c2709c7d715761d2
Skelp <skelp@pm.me>
2026-07-29 11:35:18 +0200

    Implement Rabid and Pilot creature behaviours

--- src/Toy2/Actor.cpp
+++ src/Toy2/Actor.cpp
@@ -857,6 +857,46 @@ namespace Toy2
 			}
 		}
 
+		// FUNCTION: TOY2 0x00428650 [MATCHED]
+		void Rabid(Actor::Toy2Actor::ActorBehaviourContext* context)
+		{
+			Actor::Toy2Actor* actor = context->actor;
+			actor->previousActorPhase -= (int16_t)Renderer::g_frameDelta;
+			if (actor->previousActorPhase <= 0)
+			{
+				actor->previousActorPhase = (*g_randDatBufferPtr++ & 0x7F) + 0x3C;
+				AudioManager::PlaySoundEffect(0x96, &actor->pos);
+			}
+
+			if ((actor->actorFlags & Actor::ACTOR_FLAG_INTERACTION_REQUESTED) != 0 && actor->actorPhase == 0x66)
+			{
+				g_levelObjectiveProgress++;
+				Particles::SpawnCollectSparkle(actor->pos.x, actor->pos.y - 0x2000, actor->pos.z, 0x32);
+				AudioManager::PlaySoundEffect(0x96, &actor->pos);
+				Actor::Kill(actor, 2);
+			}
+		}
+
+		// FUNCTION: TOY2 0x0042C150 [MATCHED]
+		void Pilot(Actor::Toy2Actor::ActorBehaviourContext* context)
+		{
+			Actor::Toy2Actor* actor = context->actor;
+			actor->previousActorPhase -= (int16_t)Renderer::g_frameDelta;
+			if (actor->previousActorPhase <= 0 && actor->actorPhase == 0x66)
+			{
+				actor->previousActorPhase = (*g_randDatBufferPtr++ & 0x7F) + 0x5A;
+				AudioManager::PlaySoundEffect(0x6B, &actor->pos);
+			}
+
+			if ((actor->actorFlags & Actor::ACTOR_FLAG_INTERACTION_REQUESTED) != 0 && actor->actorPhase == 0x66)
+			{
+				g_levelObjectiveProgress++;
+				Particles::SpawnCollectSparkle(actor->pos.x, actor->pos.y - 0x2000, actor->pos.z, 0x32);
+				AudioManager::PlaySoundEffect(0x6C, &actor->pos);
+				Actor::Kill(actor, 2);
+			}
+		}
+
 		// FUNCTION: TOY2 0x0041DEC0 [MATCHED]
 		void Ducks(Actor::Toy2Actor::ActorBehaviourContext* context)
 		{
