4ff9aee9c197b860ee35cc09f918f2c3179eb9e8
Skelp <skelp@pm.me>
2026-07-30 00:42:44 +0200

    Implement CreatureBehaviour::Clown

--- src/Toy2/AlleysAndGullies.cpp
+++ src/Toy2/AlleysAndGullies.cpp
@@ -2,6 +2,7 @@
 #include "Toy2/LevelLogic.h"
 #include "Toy2/Actor.h"
 #include "Toy2/Particles.h"
+#include "Toy2/Weather.h"
 #include "AudioManager/AudioManager.h"
 #include "Nu3D/Particles.h"
 #include "Renderer/Renderer.h"
@@ -10,8 +11,27 @@
 
 namespace Toy2
 {
+	extern int32_t g_hudActorAnimationFrame;
+
 	namespace AlleysAndGullies
 	{
+		enum ClownChallengeState
+		{
+			CLOWN_CHALLENGE_IDLE = 0,
+			CLOWN_CHALLENGE_INTRO = 1,
+			CLOWN_CHALLENGE_ACTIVE = 2,
+			CLOWN_CHALLENGE_COMPLETE = 200,
+		};
+
+		// GLOBAL: TOY2 0x0052F8B0
+		int32_t g_clownPhaseTimer;
+		// GLOBAL: TOY2 0x0052F8B8
+		int32_t g_previousClownPhase;
+		// GLOBAL: TOY2 0x0052F8E8
+		int32_t g_clownChallengeState;
+		// GLOBAL: TOY2 0x0052F8FC
+		int32_t g_clownTintFlashToggle;
+
 		// STUB: TOY2 0x0041E390
 		void Init() {}
 
@@ -24,8 +44,60 @@ namespace Toy2
 {
 	namespace CreatureBehaviour
 	{
-		// STUB: TOY2 0x0041DDB0
-		void Clown(Actor::Toy2Actor::ActorBehaviourContext* context) {}
+		// FUNCTION: TOY2 0x0041DDB0 [MATCHED]
+		void Clown(Actor::Toy2Actor::ActorBehaviourContext* context)
+		{
+			Actor::Toy2Actor* actor = context->actor;
+			AlleysAndGullies::g_clownTintFlashToggle = (AlleysAndGullies::g_clownTintFlashToggle - 1) & 1;
+
+			if (actor->actorPhase != AlleysAndGullies::g_previousClownPhase)
+			{
+				AlleysAndGullies::g_previousClownPhase = actor->actorPhase;
+				AlleysAndGullies::g_clownPhaseTimer = 60;
+				actor->creatureRam->defenseMode = 4;
+			}
+
+			if ((actor->actorFlags & Actor::ACTOR_FLAG_INTERACTION_REQUESTED) != 0)
+			{
+				actor->actorFlags &= ~Actor::ACTOR_FLAG_INTERACTION_REQUESTED;
+				AudioManager::PlaySoundEffect(0xA1, &actor->pos);
+			}
+
+			if (AlleysAndGullies::g_clownChallengeState > AlleysAndGullies::CLOWN_CHALLENGE_INTRO)
+			{
+				AlleysAndGullies::g_clownPhaseTimer -= Renderer::g_frameDelta;
+				if (AlleysAndGullies::g_clownPhaseTimer < 0)
+				{
+					AlleysAndGullies::g_clownPhaseTimer = 0;
+					actor->creatureRam->defenseMode = 7;
+					actor->useTint = 0;
+				}
+				else if (AlleysAndGullies::g_clownTintFlashToggle != 0)
+				{
+					actor->useTint = 1;
+					actor->actorTint.r = 0x2000;
+					actor->actorTint.g = 0x2000;
+					actor->actorTint.b = 0x2000;
+				}
+				else
+				{
+					actor->useTint = 0;
+				}
+			}
+			else
+			{
+				actor->useTint = 0;
+			}
+
+			if (AlleysAndGullies::g_clownChallengeState == AlleysAndGullies::CLOWN_CHALLENGE_ACTIVE)
+			{
+				g_hudActorAnimationFrame = actor->actorPhase * 54 / 20;
+				if (Sector::g_activeSectorIndex == 2)
+				{
+					HUD::g_slideTimers[HUD::SLIDE_BOSS_STATUS] = 90;
+				}
+			}
+		}
 
 		// FUNCTION: TOY2 0x0041DEC0 [MATCHED]
 		void Ducks(Actor::Toy2Actor::ActorBehaviourContext* context)
