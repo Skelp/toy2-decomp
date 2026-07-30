18b8a22730f8707aa9084f384d50e94b608a9b8b
Skelp <skelp@pm.me>
2026-07-30 17:38:47 +0200

    Implement RasterizeAdditiveTexturedPolygon565

--- src/SoftwareRenderer.cpp
+++ src/SoftwareRenderer.cpp
@@ -1037,8 +1037,6 @@ namespace SoftwareRenderer
 	void UnkRenderAPI13(SoftwareRenderItem* item) {}
 	// STUB: TOY2 0x00461D20
 	void UnkRenderAPI14(SoftwareRenderItem* item) {}
-	// STUB: TOY2 0x00468E90
-	void UnkRenderAPI15(SoftwareRenderItem* item) {}
 	// STUB: TOY2 0x00467080
 	void UnkRenderAPI17(SoftwareRenderItem* item) {}
 	// STUB: TOY2 0x00469900
@@ -1710,6 +1708,132 @@ namespace SoftwareRenderer
 		} while (scanlineCount != 0);
 	}
 
+	// FUNCTION: TOY2 0x00468E90 [PROVISIONAL]
+	void RasterizeAdditiveTexturedPolygon565(SoftwareRenderItem* item)
+	{
+		int32_t bottomY = item->vertices[0].y;
+		int32_t topY = bottomY;
+		if (item->vertices[1].y < topY)
+			topY = item->vertices[1].y;
+		else if (item->vertices[1].y > bottomY)
+			bottomY = item->vertices[1].y;
+
+		if (item->vertices[2].y < topY)
+			topY = item->vertices[2].y;
+		else if (item->vertices[2].y > bottomY)
+			bottomY = item->vertices[2].y;
+
+		if (item->renderFlags & SOFTWARE_RENDER_QUAD)
+		{
+			if (item->vertices[3].y < topY)
+				topY = item->vertices[3].y;
+			else if (item->vertices[3].y > bottomY)
+				bottomY = item->vertices[3].y;
+		}
+
+		if (topY < Toy2::g_screenClipTop)
+			topY = Toy2::g_screenClipTop;
+		if (bottomY > Toy2::g_screenClipBottom)
+			bottomY = Toy2::g_screenClipBottom;
+
+		int32_t scanlineCount = bottomY - topY + 1;
+		ScanlineScratch* scanline = &g_scanlineScratch[topY];
+		ClearScanlineFlags(scanline, scanlineCount);
+
+		RASTERIZE_TEXTURED_EDGE(&item->vertices[0], &item->vertices[1], edge01DoneAdditive565);
+		RASTERIZE_TEXTURED_EDGE(&item->vertices[1], &item->vertices[2], edge12DoneAdditive565);
+		if ((item->renderFlags & SOFTWARE_RENDER_QUAD) == 0)
+		{
+			RASTERIZE_TEXTURED_EDGE(&item->vertices[2], &item->vertices[0], edge20DoneAdditive565);
+		}
+		else
+		{
+			RASTERIZE_TEXTURED_EDGE(&item->vertices[2], &item->vertices[3], edge23DoneAdditive565);
+			RASTERIZE_TEXTURED_EDGE(&item->vertices[3], &item->vertices[0], edge30DoneAdditive565);
+		}
+
+		uint16_t* texture = (uint16_t*)g_softwareTextureData[item->textureIndex];
+		uint16_t* rowStart = (uint16_t*)g_lockedBackBuffer + g_backBufferPitchPixels * topY + Toy2::g_screenClipLeft;
+		do
+		{
+			if (scanline->populated != 0 && scanline->leftXFixed <= Toy2::g_screenClipRightFixed && scanline->rightXFixed >= Toy2::g_screenClipLeftFixed)
+			{
+				int32_t leftX = scanline->leftXFixed >> 10;
+				int32_t rightX = scanline->rightXFixed >> 10;
+				if (leftX == rightX)
+				{
+					uint16_t texel = texture[(scanline->leftInterpolants[0] >> 16) + (scanline->leftInterpolants[1] >> 8 & 0xFFFFFF00)];
+					if (texel != 0x7C0)
+					{
+						uint16_t& pixel = rowStart[leftX - Toy2::g_screenClipLeft];
+						int32_t red = (pixel & 0xF800) + (texel & 0xF800);
+						if (red > 0xF800)
+							red = 0xF800;
+						int32_t green = (pixel & 0x7E0) + (texel & 0x7E0);
+						if (green > 0x7E0)
+							green = 0x7E0;
+						int32_t blue = (pixel & 0x1F) + (texel & 0x1F);
+						if (blue > 0x1F)
+							blue = 0x1F;
+						pixel = (uint16_t)(red + green + blue);
+					}
+				}
+				else
+				{
+					int32_t width = rightX - leftX;
+					int32_t u = scanline->leftInterpolants[0];
+					int32_t v = scanline->leftInterpolants[1];
+					int32_t uStep = (scanline->rightInterpolants[0] - u) / width;
+					int32_t vStep = (scanline->rightInterpolants[1] - v) / width;
+					int32_t pixelCount = width;
+					uint16_t* pixel;
+					if (leftX < Toy2::g_screenClipLeft)
+					{
+						int32_t clippedPixels = Toy2::g_screenClipLeft - leftX;
+						u += clippedPixels * uStep;
+						v += clippedPixels * vStep;
+						pixelCount = Toy2::g_softWindowWidth;
+						pixel = rowStart;
+						if (rightX <= Toy2::g_screenClipRight)
+							pixelCount = rightX - Toy2::g_screenClipLeft + 1;
+					}
+					else
+					{
+						if (rightX > Toy2::g_screenClipRight)
+							pixelCount = Toy2::g_screenClipRight - leftX;
+						pixelCount++;
+						pixel = rowStart + leftX - Toy2::g_screenClipLeft;
+					}
+
+					do
+					{
+						uint16_t texel = texture[(u >> 16) + (v >> 8 & 0xFFFFFF00)];
+						if (texel != 0x7C0)
+						{
+							int32_t red = (*pixel & 0xF800) + (texel & 0xF800);
+							if (red > 0xF800)
+								red = 0xF800;
+							int32_t green = (*pixel & 0x7E0) + (texel & 0x7E0);
+							if (green > 0x7E0)
+								green = 0x7E0;
+							int32_t blue = (*pixel & 0x1F) + (texel & 0x1F);
+							if (blue > 0x1F)
+								blue = 0x1F;
+							*pixel = (uint16_t)(red + green + blue);
+						}
+						u += uStep;
+						v += vStep;
+						pixel++;
+						pixelCount--;
+					} while (pixelCount > 0);
+				}
+			}
+			scanline++;
+			rowStart += g_backBufferPitchPixels;
+			scanlineCount--;
+		} while (scanlineCount != 0);
+	}
+
 	// FUNCTION: TOY2 0x00476D00 [PROVISIONAL]
 	void RasterizeAdditiveTexturedPolygon8(SoftwareRenderItem* item)
 	{
@@ -1953,7 +2077,7 @@ namespace SoftwareRenderer
 	SoftwareRenderDispatchTable g_softwareRenderDispatch565 = {
 		UnkRenderAPI14,
 		RasterizeSolidQuad16,
-		UnkRenderAPI15,
+		RasterizeAdditiveTexturedPolygon565,
 		RasterizeSubtractiveTexturedPolygon565,
 		{ UnkRenderAPI17, UnkRenderAPI18, UnkRenderAPI19, UnkRenderAPI20 },
 		{ UnkRenderAPI21, UnkRenderAPI22, UnkRenderAPI23, UnkRenderAPI24 },
