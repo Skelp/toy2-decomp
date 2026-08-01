524206acc4bbbd4d23590a6f12339ba09e124ac0
Skelp <skelp@pm.me>
2026-08-01 02:32:06 +0200

    Reconstruct texture pixel buffer allocation

--- src/D3DApp/texture.cpp
+++ src/D3DApp/texture.cpp
@@ -1,4 +1,21 @@
 #include "D3DApp/d3dappi.h"
+#include "Logger.h"
+#include "SoftwareRenderer.h"
+#include "Toy2/Direct6.h"
+
+#include <stdlib.h>
+
+struct TextureDimensions
+{
+	int16_t width;
+	int16_t height;
+};
+STATIC_ASSERT(sizeof(TextureDimensions) == 4);
+
+namespace SoftwareRenderer
+{
+	extern void* g_softwareTextureData[64];
+}
 
 // GLOBAL: TOY2 0x0051A840
 int32_t g_masterTextureTypes[64];
@@ -23,6 +40,84 @@ int32_t g_masterTexturePaletteState[32];
 
 // GLOBAL: TOY2 0x0050A690
 int32_t g_texturePaletteState[32];
+
+// GLOBAL: TOY2 0x0050AA64
+TextureDimensions g_textureDimensions[64];
+
+// FUNCTION: TOY2 0x00497DB0 [MATCHED]
+void AllocateTexturePixelBuffer(int16_t textureIndex)
+{
+	int32_t index = textureIndex;
+	if (g_textureData[index] != NULL || textureIndex >= 0x40)
+		return;
+
+	switch (g_renderMode)
+	{
+		case RENDERMODE_SOFTWARE:
+			if (SoftwareRenderer::g_bitsPerPixel != 8)
+			{
+				if (SoftwareRenderer::g_bitsPerPixel > 14 && SoftwareRenderer::g_bitsPerPixel <= 16)
+				{
+					uint16_t* pixelData = (uint16_t*)malloc(0x10000 * sizeof(uint16_t));
+					g_textureData[index] = pixelData;
+					SoftwareRenderer::g_softwareTextureData[index] = pixelData;
+
+					int32_t pixelsRemaining = 0x10000;
+					do
+					{
+						*pixelData++ = (0 << SoftwareRenderer::g_redShift) | (31 << SoftwareRenderer::g_greenShift);
+					} while (--pixelsRemaining != 0);
+				}
+			}
+			else
+			{
+				uint16_t* pixelData = (uint16_t*)malloc(g_textureDimensions[index].height * g_textureDimensions[index].width * sizeof(uint16_t));
+				g_textureData[index] = pixelData;
+				SoftwareRenderer::g_softwareTextureData[index] = pixelData;
+
+				for (int32_t pixelIndex = 0; pixelIndex < g_textureDimensions[index].height * g_textureDimensions[index].width; ++pixelIndex)
+				{
+					*pixelData++ = 0;
+				}
+			}
+
+			Logger::Log("MEM : SOFT Pixel buffer for tpage %d created.\n", index);
+			break;
+
+		case RENDERMODE_D3D:
+			switch (g_textureFlags[index])
+			{
+				case 0: {
+					uint8_t* pixelData = (uint8_t*)malloc(g_textureDimensions[index].height * g_textureDimensions[index].width * 3);
+					g_textureData[index] = pixelData;
+					SoftwareRenderer::g_softwareTextureData[index] = pixelData;
+
+					for (int32_t pixelIndex = 0; pixelIndex < g_textureDimensions[index].height * g_textureDimensions[index].width; ++pixelIndex)
+					{
+						*pixelData++ = 0;
+						*pixelData++ = 0xFF;
+						*pixelData++ = 0;
+					}
+					break;
+				}
+
+				case 1: {
+					uint8_t* pixelData = (uint8_t*)malloc(g_textureDimensions[index].height * g_textureDimensions[index].width);
+					g_textureData[index] = pixelData;
+					SoftwareRenderer::g_softwareTextureData[index] = pixelData;
+
+					for (int32_t pixelIndex = 0; pixelIndex < g_textureDimensions[index].height * g_textureDimensions[index].width; ++pixelIndex)
+					{
+						*pixelData++ = 0;
+					}
+					break;
+				}
+			}
+
+			Logger::Log("MEM : D3D Pixel buffer for tpage %d created.\n", index);
+			break;
+	}
+}
 
 // FUNCTION: TOY2 0x00409C80 [MATCHED]
 BOOL D3DAppIReleaseAllTextures() { return TRUE; }
