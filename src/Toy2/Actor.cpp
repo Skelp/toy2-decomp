19e168d2116b1dfd2edb9050beab749a781512f1
Skelp <skelp@pm.me>
2026-08-02 15:02:47 +0200

    Refine item return unlock flag

--- src/Toy2/Actor.cpp
+++ src/Toy2/Actor.cpp
@@ -793,7 +793,7 @@ namespace Toy2
 			Dialogue::Begin(actorIndex, dialogueRecordIndex, itemReturnedSubtitle, actorFacingAngle, cameraFacingAngle, -1);
 
 			Gadget::g_unlockNodeState = 0;
-			uint8_t unlockFlag = Gadget::g_levelUnlockInfo[g_levelFileIndex - 1].unlockFlag;
+			int32_t unlockFlag = Gadget::g_levelUnlockInfo[g_levelFileIndex - 1].unlockFlag;
 			g_unlocks |= unlockFlag;
 			switch (unlockFlag)
 			{
